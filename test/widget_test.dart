a2450ac4c096a6dd73d9d79a0f0c3668cd7f30b6
kimrunar <kimrunar_3@hotmail.com>
2021-10-20 12:01:09 +0200

    fixed failing test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mtime_mordre/Home.dart';
 
 import 'package:mtime_mordre/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Mordre_Mtime());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
